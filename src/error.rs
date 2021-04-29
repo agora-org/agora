bcf87b8378491c9a9f280309412b58f2882cc39a
Sönke Hahn <soenkehahn@gmail.com>
2021-04-29 19:13:12 -0400

    Add display messages for all error variants

--- src/error.rs
+++ src/error.rs
@@ -8,27 +8,21 @@ pub(crate) type Result<T, E = Error> = std::result::Result<T, E>;
 #[derive(Debug, Snafu)]
 #[snafu(visibility = "pub(crate)")]
 pub(crate) enum Error {
-  #[snafu(context(false))]
-  Clap {
-    source: clap::Error,
-  },
+  #[snafu(
+    context(false),
+    display("Failed to parse command line arguments: {}", source)
+  )]
+  Clap { source: clap::Error },
   #[snafu(display("IO error accessing `www`: {}", source))]
-  WwwIo {
-    source: io::Error,
-  },
-  FileIo {
-    source: io::Error,
-  },
+  WwwIo { source: io::Error },
+  #[snafu(display("IO error accessing file: {}", source))]
+  FileIo { source: io::Error },
   #[snafu(display("Invalid URL file path: {}", uri))]
-  InvalidPath {
-    uri: Uri,
-  },
-  CurrentDir {
-    source: io::Error,
-  },
-  ServerRun {
-    source: hyper::Error,
-  },
+  InvalidPath { uri: Uri },
+  #[snafu(display("Failed to retrieve current directory: {}", source))]
+  CurrentDir { source: io::Error },
+  #[snafu(display("Failed running HTTP server: {}", source))]
+  ServerRun { source: hyper::Error },
 }
 
 impl Error {
