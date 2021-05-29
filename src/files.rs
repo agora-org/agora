59b5ea625e9a0259616272760191f865e09d12ce
Sönke Hahn <soenkehahn@gmail.com>
2021-05-29 17:19:04 -0400

    Remove unnecessary parenthesis

--- src/files.rs
+++ src/files.rs
@@ -106,8 +106,8 @@ impl Files {
           link rel="stylesheet" href="/static/index.css";
         }
         body {
-          ul class=("contents") {
-            li class=("separator") {}
+          ul class="contents" {
+            li class="separator" {}
             @for (file_name, file_type) in Self::read_dir(dir).await? {
               @let file_name = {
                 let mut file_name = file_name.to_string_lossy().into_owned();
@@ -117,17 +117,17 @@ impl Files {
                 file_name
               };
               @let encoded = percent_encoding::utf8_percent_encode(&file_name, &Self::ENCODE_CHARACTERS);
-              li class=("file") {
-                a href=(encoded) class=("view") {
+              li class="file" {
+                a href=(encoded) class="view" {
                   (file_name)
                 }
                 @if file_type.is_file() {
-                  a download href=(encoded) class=("download") {
+                  a download href=(encoded) class="download" {
                     img src=("/static/download.svg");
                   }
                 }
               }
-              li class=("separator") {}
+              li class="separator" {}
             }
           }
         }
