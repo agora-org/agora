753f568fd9496b2628e21f829ff0e0cc855e0e93
Sönke Hahn <soenkehahn@gmail.com>
2021-08-29 11:04:19 -0600

    Move backtrace test to smoke tests

--- tests/smoke.rs
+++ tests/smoke.rs
@@ -1,42 +1,87 @@
 use executable_path::executable_path;
 use hyper::StatusCode;
+use reqwest::Url;
 use std::{
   fs,
-  io::{BufRead, BufReader},
-  process::{Command, Stdio},
+  io::{BufRead, BufReader, Read},
+  process::{Child, ChildStderr, Command, Stdio},
 };
+use tempfile::TempDir;
+
+struct AgoraInstance {
+  _tempdir: TempDir,
+  child: Child,
+  port: u16,
+  stderr: ChildStderr,
+  collected_stderr: String,
+}
+
+impl AgoraInstance {
+  fn new(tempdir: TempDir) -> Self {
+    let mut child = Command::new(executable_path("agora"))
+      .arg("--port=0")
+      .arg("--directory=.")
+      .current_dir(&tempdir)
+      .stderr(Stdio::piped())
+      .spawn()
+      .unwrap();
+
+    let mut first_line = String::new();
+    let child_stderr = child.stderr.take().unwrap();
+    let mut child_stderr = BufReader::new(child_stderr);
+    child_stderr.read_line(&mut first_line).unwrap();
+    let port: u16 = first_line
+      .strip_prefix("Listening on 0.0.0.0:")
+      .unwrap()
+      .trim_end()
+      .parse()
+      .unwrap();
+
+    AgoraInstance {
+      _tempdir: tempdir,
+      child,
+      port,
+      collected_stderr: first_line,
+      stderr: child_stderr.into_inner(),
+    }
+  }
+
+  fn base_url(&self) -> Url {
+    Url::parse(&format!("http://localhost:{}", self.port)).unwrap()
+  }
+
+  fn kill(mut self) -> String {
+    self.child.kill().unwrap();
+    self.child.wait().unwrap();
+    self
+      .stderr
+      .read_to_string(&mut self.collected_stderr)
+      .unwrap();
+    self.collected_stderr
+  }
+}
 
 #[test]
 fn server_listens_on_all_ip_addresses() {
   let tempdir = tempfile::tempdir().unwrap();
+  let agora = AgoraInstance::new(tempdir);
+  assert_eq!(
+    reqwest::blocking::get(agora.base_url()).unwrap().status(),
+    StatusCode::OK
+  );
+  let stderr = agora.kill();
+  assert!(stderr.contains("0.0.0.0:"));
+}
 
-  fs::create_dir(tempdir.path().join("www")).unwrap();
-
-  let mut child = Command::new(executable_path("agora"))
-    .arg("--port=8080")
-    .arg("--directory=www")
-    .current_dir(&tempdir)
-    .stderr(Stdio::piped())
-    .spawn()
-    .unwrap();
-
-  let child_stderr = child.stderr.take().unwrap();
-  let result = std::panic::catch_unwind(|| {
-    let mut line = String::new();
-
-    BufReader::new(child_stderr).read_line(&mut line).unwrap();
-    eprintln!("stderr: {}", line);
-
-    assert!(line.contains("0.0.0.0:8080"));
-
-    assert_eq!(
-      reqwest::blocking::get("http://localhost:8080")
-        .unwrap()
-        .status(),
-      StatusCode::OK
-    );
-  });
-  child.kill().unwrap();
-  child.wait().unwrap();
-  result.unwrap();
+#[test]
+fn errors_contain_backtraces() {
+  let tempdir = tempfile::tempdir().unwrap();
+  fs::write(tempdir.path().join(".hidden"), "").unwrap();
+  let agora = AgoraInstance::new(tempdir);
+  let status = reqwest::blocking::get(agora.base_url().join("/files/.hidden").unwrap())
+    .unwrap()
+    .status();
+  assert_eq!(status, StatusCode::NOT_FOUND);
+  let rest = agora.kill();
+  assert!(rest.contains("agora::files::Files::check_path"));
 }
