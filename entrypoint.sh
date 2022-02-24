a3fbe2876e6639417870944cbb5f042568b99878
yzernik <yzernik@gmail.com>
2022-02-24 04:16:16 -0800

    Got docker image working with lnd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 
+echo $LND_RPC_AUTHORITY
+echo $AGORA_PORT
+echo $FILES_DIR
+echo $TLS_CERT_PATH
+echo $INVOICES_MACAROON_PATH
+
+sleep 120
+
 # if lnd enabled, attempt to connect
 if [[ ! -z "${LND_RPC_AUTHORITY}" ]]
 then
-    exec agora --directory $FILES_DIR --http-port $AGORA_PORT --lnd-rpc-authority $LND_RPC_AUTHORITY --lnd-rpc-cert-path .lnd/tls.cert --lnd-rpc-macaroon-path .lnd/invoices.macaroon
+    exec agora --directory $FILES_DIR --http-port $AGORA_PORT --lnd-rpc-authority $LND_RPC_AUTHORITY --lnd-rpc-cert-path $TLS_CERT_PATH --lnd-rpc-macaroon-path $INVOICES_MACAROON_PATH
 # else run simple server
 else
     exec agora --directory $FILES_DIR --http-port $AGORA_PORT
