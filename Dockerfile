48d3205ac342b4951534e6c872f185a26485d3a0
Jonathan Zernik <yzernik@gmail.com>
2022-02-26 20:49:23 -0800

    Add Dockerfile (#291)

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM rust:1.59.0-buster AS builder
+
+WORKDIR /app
+
+COPY . ./
+
+RUN cargo build --release
+
+FROM debian:buster-slim
+
+COPY --from=builder /app/target/release/agora /usr/local/bin/agora
+
+COPY "entrypoint.sh" .
+RUN chmod +x entrypoint.sh
+
+CMD ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# if lnd enabled, attempt to connect
+if [[ ! -z "${LND_RPC_AUTHORITY}" ]]; then
+    exec agora \
+	 --directory $FILES_DIR \
+	 --http-port $AGORA_PORT \
+	 --lnd-rpc-authority $LND_RPC_AUTHORITY \
+	 --lnd-rpc-cert-path $TLS_CERT_PATH \
+	 --lnd-rpc-macaroon-path $INVOICES_MACAROON_PATH
+# else run simple server
+else
+    exec agora \
+	 --directory $FILES_DIR \
+	 --http-port $AGORA_PORT
+fi
