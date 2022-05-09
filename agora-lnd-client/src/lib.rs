dbfd4c5623df92dbb54fdcd8c131105cd68285ea
yzernik <yzernik@gmail.com>
2022-05-09 16:59:44 -0700

    Organize lib module

--- agora-lnd-client/src/lib.rs
+++ agora-lnd-client/src/lib.rs
@@ -1,14 +1,8 @@
-
-
-// mod https_service;
 mod https_service;
 mod millisatoshi;
 mod lnd;
 mod invoice;
 
-
 pub use millisatoshi::Millisatoshi;
-
 pub use lnd::Client;
-
 pub use invoice::LightningInvoice;
