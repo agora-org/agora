e13bb55c5fd48f0f0a4a896c28b3de2a97fa0094
Jonathan Zernik <yzernik@gmail.com>
2022-05-22 09:38:23 -0700

    Partial implementation of add invoice method for core lightning

--- agora-lnd-client/src/core_lightning.rs
+++ agora-lnd-client/src/core_lightning.rs
@@ -2,9 +2,11 @@ use {
   crate::millisatoshi::Millisatoshi,
   crate::LightningError,
   crate::LightningInvoice,
+  crate::AddLightningInvoiceResponse,
   crate::LightningNodeClient,
   clightningrpc::LightningRPC,
   clightningrpc::responses::ListInvoice,
+  clightningrpc::responses::Invoice,
   async_trait::async_trait,
   std::str,
   futures::future,
@@ -35,6 +37,15 @@ impl LightningInvoice for ListInvoice {
 }
 
 
+impl AddLightningInvoiceResponse for Invoice {
+
+    fn payment_hash(&self) -> &Vec<u8> {
+        &self.payment_hash.as_bytes().to_vec()
+    }
+
+}
+
+
 
 #[derive(Debug, Clone)]
 pub struct CoreLightningClient {
@@ -48,40 +59,49 @@ impl LightningNodeClient for CoreLightningClient {
 
   async fn ping(&self) -> Result<(), LightningError> {
 
-    let mut client = LightningRPC::new(&self.inner);
+    let client = LightningRPC::new(&self.inner);
 
     let list_invoices_resp = client.getinfo();
 
     match list_invoices_resp {
         Ok(_) => future::ok(()),
-        Err(e) => future::err(LightningError),
+        Err(_) => future::err(LightningError),
     }.await
 
   }
 
-  // async fn add_invoice(
-  //   &self,
-  //   memo: &str,
-  //   value_msat: Millisatoshi,
-  // ) -> Result<Box<dyn AddLightningInvoiceResponse + Send>, LightningError> {
-  //   let request = tonic::Request::new(Invoice {
-  //     memo: memo.to_owned(),
-  //     value_msat: value_msat.value().try_into().map_err(|source| {
-  //       Status::new(
-  //         Code::InvalidArgument,
-  //         format!("invalid value for `value_msat`: {}", source),
-  //       )
-  //     })?,
-  //     ..Invoice::default()
-  //   });
-  //   Ok(Box::new(self.clone().inner.add_invoice(request).await?.into_inner()))
-  // }
+  async fn add_invoice(
+    &self,
+    memo: &str,
+    value_msat: Millisatoshi,
+  ) -> Result<Box<dyn AddLightningInvoiceResponse + Send>, LightningError> {
+
+      let value_msat_num = value_msat.value();
+
+      let client = LightningRPC::new(&self.inner);
+
+      let invoice_res = client.invoice(
+	  value_msat_num,
+	  memo,
+	  "",
+	  None,
+      );
+
+      match invoice_res {
+          Ok(invoice_resp) => {
+	      let boxed_invoice = Box::new(invoice_resp) as _;
+	      future::ok(boxed_invoice)
+	  },
+          Err(_) => future::err(LightningError),
+      }.await
+
+  }
 
   async fn lookup_invoice(&self, r_hash: [u8; 32]) -> Result<Option<Box<dyn LightningInvoice + Send>>, LightningError> {
 
       let payment_hash_str = str::from_utf8(&r_hash).unwrap();
 
-      let mut client = LightningRPC::new(&self.inner);
+      let client = LightningRPC::new(&self.inner);
 
       let list_invoices_res = client.listinvoices(Some(payment_hash_str));
 
@@ -89,21 +109,11 @@ impl LightningNodeClient for CoreLightningClient {
           Ok(list_invoices_resp) => {
 	      let invoices = list_invoices_resp.invoices;
 	      let maybe_invoice = invoices.get(0);
-	      let boxed_maybe = maybe_invoice.map(|inv| Box::new(*inv) as _);
+	      let boxed_maybe = maybe_invoice.map(|inv| Box::new(inv.clone()) as _);
 	      future::ok(boxed_maybe)
 	  },
-          Err(e) => future::err(LightningError),
+          Err(_) => future::err(LightningError),
       }.await
-
-      // future::ok(list_invoices_resp.unwrap()).await
-
-      // match list_invoices_resp.unwrap() {
-      // 	  clightningrpc::responses::ListInvoices { invoices } => "fooooo"
-      // };
-
-      //if invoices.contains(0)
-
-      //future::err(LightningError).await
 
   }
 
